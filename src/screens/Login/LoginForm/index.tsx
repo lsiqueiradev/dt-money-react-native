e33b882af4bf1576afc7fc467b261fff93667cc3
Lucas Siqueira <lucas@lsiqueira.dev>
2025-07-21 18:58:32 -0300

    feat: Finalizando interface da tela de Login

--- src/screens/Login/LoginForm/index.tsx
+++ src/screens/Login/LoginForm/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
 import { useForm } from "react-hook-form";
+import { Text, View } from "react-native";
 
 export interface FormLoginParams {
   email: string;
@@ -32,10 +33,18 @@ export function LoginForm() {
         leftIconName="lock-outline"
         secureTextEntry
       />
+      <View className="flex-1 justify-between mt-8 mb-6 min-h-[250px]">
+        <Button iconName="arrow-forward">Login</Button>
 
-      <Button iconName="arrow-forward" mode="outline">
-        Login
-      </Button>
+        <View>
+          <Text className="mb-6 text-gray-300 text-base">
+            Ainda não possui uma conta?
+          </Text>
+          <Button iconName="arrow-forward" mode="outline">
+            Cadastrar
+          </Button>
+        </View>
+      </View>
     </>
   );
 }
