cef4921a5a65a46d8be62d11ecf69db62400a0fa
Lucas Siqueira <lucas@lsiqueira.dev>
2025-07-22 14:00:51 -0300

    feat: criando o Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,29 @@
+import { Image, Text, TouchableOpacity, View } from "react-native";
+
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { useAuthContext } from "@/contexts/auth.context";
+
+export function Header() {
+  const { handleLogout } = useAuthContext();
+  return (
+    <View className="w-full flex-row p-8 items-center justify-between">
+      <View>
+        <Image
+          source={require("@/assets/Logo.png")}
+          className="w-[130px] h-[30px]"
+        />
+        <TouchableOpacity
+          onPress={handleLogout}
+          className="flex-row items-center gap-2 mt-2"
+        >
+          <MaterialIcons name="logout" className="text-gray-700" size={15} />
+          <Text className="text-gray-700 text-base">Sair da conta</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity className="bg-accent-brand w-[130px] items-center justify-center rounded-xl h-[50px]">
+        <Text className="text-white font-bold text-sm">Nova transação</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,15 @@
+import { Header } from "@/components/Header";
 import { useAuthContext } from "@/contexts/auth.context";
-import { Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView, Text, TouchableOpacity } from "react-native";
 
 export function Home() {
   const { handleLogout } = useAuthContext();
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Tela de Home</Text>
-
+    <SafeAreaView className="flex-1 bg-background-primary">
+      <Header />
       <TouchableOpacity onPress={handleLogout}>
         <Text>Sair</Text>
       </TouchableOpacity>
-    </View>
+    </SafeAreaView>
   );
 }
